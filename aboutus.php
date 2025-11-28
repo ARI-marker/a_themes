8de915413101e8fb6001a0810191a32b8849680b
ARI-marker <arisheqiri833@gmail.com>
2025-11-28 19:46:32 +0100

    addad

--- aboutus.php
+++ aboutus.php
@@ -3,9 +3,55 @@
 Template Name: About Us
 Template Post Type: page
 */
-get_header();
-?>
-<div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+get_header();?>
+
+                                                <section class="py-24 relative">
+        <div class="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div class="w-full justify-start items-center gap-12 grid lg:grid-cols-2 grid-cols-1">
+                <div
+                    class="w-full justify-center items-start gap-6 grid sm:grid-cols-2 grid-cols-1 lg:order-first order-last">
+                    <div class="pt-24 lg:justify-center sm:justify-end justify-start items-start gap-2.5 flex">
+                        <img class=" rounded-xl object-cover" src="https://pagedone.io/asset/uploads/1717741205.png" alt="about Us image" />
+                    </div>
+                    <img class="sm:ml-0 ml-auto rounded-xl object-cover" src="https://pagedone.io/asset/uploads/1717741215.png"
+                        alt="about Us image" />
+                </div>
+                <div class="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
+                    <div class="w-full flex-col justify-center items-start gap-8 flex">
+                        <div class="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
+                            <h2
+                                class="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
+                                Empowering Each Other to Succeed</h2>
+                            <p class="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">
+                                Every project we've undertaken has been a collaborative effort, where every person
+                                involved has left their mark. Together, we've not only constructed buildings but also
+                                built enduring connections that define our success story.</p>
+                        </div>
+                        <div class="w-full lg:justify-start justify-center items-center sm:gap-10 gap-5 inline-flex">
+                            <div class="flex-col justify-start items-start inline-flex">
+                                <h3 class="text-gray-900 text-4xl font-bold font-manrope leading-normal">33+</h3>
+                                <h6 class="text-gray-500 text-base font-normal leading-relaxed">Years of Experience</h6>
+                            </div>
+                            <div class="flex-col justify-start items-start inline-flex">
+                                <h4 class="text-gray-900 text-4xl font-bold font-manrope leading-normal">125+</h4>
+                                <h6 class="text-gray-500 text-base font-normal leading-relaxed">Successful Projects</h6>
+                            </div>
+                            <div class="flex-col justify-start items-start inline-flex">
+                                <h4 class="text-gray-900 text-4xl font-bold font-manrope leading-normal">52+</h4>
+                                <h6 class="text-gray-500 text-base font-normal leading-relaxed">Happy Clients</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        class="sm:w-fit w-full px-3.5 py-2 bg-indigo-600 hover:bg-indigo-800 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                        <span class="px-1.5 text-white text-sm font-medium leading-6">Read More</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+                                            
+<!-- <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
   <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center" />
   <div aria-hidden="true" class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl">
     <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"></div>
@@ -45,6 +91,6 @@ get_header();
       </dl>
     </div>
   </div>
-</div>
+</div> -->
 
 <?php get_footer(); ?>
